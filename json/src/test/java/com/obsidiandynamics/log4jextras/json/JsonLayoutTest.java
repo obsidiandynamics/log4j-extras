168031d3b2a5cb87561e9c479936fc16263d8505
Emil Koutanov <ekoutanov@gmail.com>
2017-10-18 20:06:46 +1100

    Added JSON layout and Splunk appender

---
+++ json/src/test/java/com/obsidiandynamics/log4jextras/json/JsonLayoutTest.java
@@ -0,0 +1,121 @@
+package com.obsidiandynamics.log4jextras.json;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.*;
+import org.junit.*;
+import com.obsidiandynamics.log4jextras.*;
+
+/**
+ *  Adapted from https://github.com/michaeltandy/log4j-json.
+ */
+public final class JsonLayoutTest {
+  private static Logger LOG;
+  
+  @BeforeClass
+  public static void beforeClass() {
+    System.setProperty("log4j.configuration", "log4j-test.properties");
+    LOG = Logger.getLogger(JsonLayoutTest.class);
+  }
+  
+  @AfterClass
+  public static void afterClass() {
+    System.clearProperty("log4j.configuration");
+    LOG = null;
+  }
+  
+  @Before
+  public void before() {
+    TestAppender.baos.reset();
+    MDC.clear();
+  }
+
+  @Test
+  public void testDemonstration() {
+    LOG.info("Example of some logging");
+    LOG.warn("Some text\nwith a newline", new Exception("Outer Exception", new Exception("Nested Exception")));
+    LOG.fatal("Text may be complicated & have many symbols\nÂ¬!Â£$%^&*()_+{}:@~<>?,./;'#[]-=`\\| \t\n");
+
+    final String whatWasLogged = TestAppender.baos.toString();
+    final String[] lines = whatWasLogged.split("\n");
+
+    assertEquals(3,lines.length);
+    assertTrue(lines[0].contains("INFO"));
+    assertTrue(lines[0].contains("Example of some logging"));
+
+    assertTrue(lines[1].contains("newline"));
+    assertTrue(lines[1].contains("Outer Exception"));
+    assertTrue(lines[1].contains("Nested Exception"));
+
+    assertTrue(lines[2].contains("have many symbols"));
+  }
+
+  @Test
+  public void testObjectHandling() {
+    LOG.info(new Object() {
+      @Override public String toString() {
+        throw new RuntimeException("Hypothetical failure");
+      }
+    });
+    LOG.warn(null);
+
+    final String whatWasLogged = TestAppender.baos.toString();
+    final String[] lines = whatWasLogged.split("\n");
+    assertEquals(2,lines.length);
+
+    assertTrue(lines[0].contains("Hypothetical"));
+    assertTrue(lines[1].contains("WARN"));
+  }
+
+  @Test
+  public void testLogMethod() {
+    // Test for https://github.com/michaeltandy/log4j-json/issues/1
+    LOG.log("asdf", Level.INFO, "this is the log message", null);
+
+    final String whatWasLogged = TestAppender.baos.toString();
+    final String[] lines = whatWasLogged.split("\n");
+    assertEquals(1,lines.length);
+    assertTrue(lines[0].contains("this is the log message"));
+  }
+
+  @Test
+  public void testMinimumLevelForSlowLogging() {
+    LOG.info("Info level logging");
+    LOG.debug("Debug level logging");
+
+    final String whatWasLogged = TestAppender.baos.toString();
+    final String[] lines = whatWasLogged.split("\n");
+    assertEquals(2,lines.length);
+
+    assertTrue(lines[0].contains("INFO"));
+    assertTrue(lines[0].contains("class"));
+    assertTrue(lines[0].contains("line"));
+    assertTrue(lines[0].contains("method"));
+
+    assertTrue(lines[1].contains("DEBUG"));
+    assertFalse(lines[1].contains("class"));
+    assertFalse(lines[1].contains("line"));
+    assertFalse(lines[1].contains("method"));
+  }
+
+  @Test
+  public void testSelectiveMdcLogging() {
+    MDC.put("asdf", "value_for_key_asdf");
+    MDC.put("qwer", "value_for_key_qwer");
+    MDC.put("thread", "attempt to overwrite thread in output");
+
+    LOG.info("Example of some logging");
+
+    MDC.clear();
+
+    final String whatWasLogged = TestAppender.baos.toString();
+    final String[] lines = whatWasLogged.split("\n");
+
+    assertEquals(1,lines.length);
+    assertTrue(lines[0].contains("value_for_key_asdf"));
+    assertFalse(lines[0].contains("value_for_key_qwer"));
+
+    assertTrue(lines[0].contains("thread"));
+    assertFalse(lines[0].contains("attempt to overwrite thread in output"));
+  }
+}
